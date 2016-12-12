6c42cffda8d3fa65c8f5962bbc51cf0ecd708ca3
cucazer <cucazik@inbox.lv>
2016-12-12 15:44:23 +0100

    Class renamings, coordinate fixes

--- qwerty/combatMap.cs
+++ qwerty/combatMap.cs
@@ -7,28 +7,23 @@ using System.Threading.Tasks;
 
 namespace qwerty
 {
-    class combatMap
+    class CombatMap
     {
         public int width;
         public int height;
         public int scale = 3;
-        public List<Box> boxes = new List<Box>();
-        public int deltax; 
-        public int deltay; 
-        public combatMap(int w, int h) 
+        public List<Cell> boxes = new List<Cell>(); 
+        public CombatMap(int w, int h) 
         {
             width = w;
             height = h;
 
-            deltax = 20 * scale;
-            deltay = 20 * scale;
-
             InitializeMap();
         }
 
-        public Box getBoxByCoords(int x, int y)
+        public Cell getBoxByCoords(int x, int y)
         {
-            Box targetBox = null;
+            Cell targetBox = null;
             for (int i = 0; i < boxes.Count; i++ )
             {
                 if (boxes[i].x == x && boxes[i].y == y)
@@ -56,12 +51,11 @@ namespace qwerty
                     {
                         // нечетная
                         if (j % height == 0) ycoord = 1;
-                        Box box = new Box(scale, i, j, new Size(scale + 10,(int)(Math.Sin(Math.PI/3) * scale + 10)));
+                        Cell box = new Cell(scale, i, j, new Size(scale + 10,(int)(Math.Sin(Math.PI/3) * scale + 10)));
                         
-                        box.x = xcoord;
-                        box.y = ycoord;
+                        //box.x = xcoord;
+                        //box.y = ycoord;
                         box.id = count++;
-                        box.centerDetermine();
 
                         boxes.Add(box);
 
@@ -70,12 +64,11 @@ namespace qwerty
                     {
                         // четная
                         if (j % height == 0) ycoord = 0;
-                        Box box = new Box(scale, i, j, new Size(scale + 10, (int)(Math.Sin(Math.PI / 3) * scale + 10)));
+                        Cell box = new Cell(scale, i, j, new Size(scale + 10, (int)(Math.Sin(Math.PI / 3) * scale + 10)));
 
-                        box.x = xcoord;
-                        box.y = ycoord;
+                        //box.x = xcoord;
+                        //box.y = ycoord;
                         box.id = count++;
-                        box.centerDetermine();
 
                         boxes.Add(box);
                     }
