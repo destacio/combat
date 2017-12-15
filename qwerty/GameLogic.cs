a646ecbc4432e82ca84ef4feb7cc66b5218764d9
Konstantins Franckevics <cucazik@inbox.lv>
2017-12-15 02:36:54 +0100

    Added game logic class and player enum

---
+++ qwerty/GameLogic.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using Barbar.HexGrid;
+using qwerty.Objects;
+using Point = System.Drawing.Point;
+
+namespace qwerty
+{
+    public class GameLogic
+    {
+        private Ship activeShip;
+        private readonly ObjectManager objectManager;
+        private Player activePlayer = Player.FirstPlayer;
+
+        public GameLogic(int fieldWidth, int fieldHeight)
+        {
+            objectManager = new ObjectManager(fieldWidth, fieldHeight);
+        }
+
+        public void HandleFieldClick(Point clickLocation)
+        {
+            OffsetCoordinates clickedHexagon;
+            SpaceObject clickedObject;
+            try
+            {
+                clickedHexagon = objectManager.PixelToOffsetCoordinates(clickLocation);
+                clickedObject = objectManager.PixelToSpaceObject(clickLocation);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // clicked pixel outside game field
+                return;
+            }
+
+            if (activeShip == null)
+            {
+                // Nothing active and nothing to be activated
+                if (clickedObject == null) return;
+
+                if (activePlayer == clickedObject.player)
+                {
+                    activeShip = (Ship) clickedObject;
+                }
+                //boxDescription.Text = clickedObject.Description;
+                //UpdateUi();
+                return;
+            }
+            
+            // если выбранная клетка пуста - определяем возможность перемещения 
+            if (clickedObject == null)
+            {
+                MoveActiveShip(clickedHexagon);
+                return;
+            }
+            
+            
+            if (clickedObject.player == activePlayer)
+            {
+                //boxDescription.Text = clickedObject.Description;
+                activeShip = (Ship) clickedObject;
+
+                //UpdateUi();
+            }
+            else
+            {
+                ActiveShipAttack(clickedObject);
+            }
+        }
+
+        private void ActiveShipAttack(SpaceObject enemyObject)
+        {
+            if (activeShip.EquippedWeapon.attackRange < objectManager.GetDistance(activeShip, enemyObject) ||
+                activeShip.actionsLeft < activeShip.EquippedWeapon.energyСonsumption)
+            {
+                // another object is out of range or requires more energy than is left
+                return;
+            }
+
+            var angle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
+
+            // поворачиваем корабль на угол angle
+            RotateShip(angle);
+
+            // отрисовка атаки
+            Thread.Sleep(150);
+
+            if (activeShip.Attack(cMap, selectedCell.id, ref combatBitmap, player, ref pictureMap))
+                UpdateShipCount();
+            UpdateUi();
+
+            // возвращаем корабль в исходное положение
+            RotateShip(-angle);
+
+            // убираем подсветку с корабля, если у него не осталось очков передвижений
+            if (activeShip.actionsLeft == 0)
+            {
+                activeShip = null;
+                UpdateUi();
+            }
+        }
+
+        private void MoveActiveShip(OffsetCoordinates clickedHexagon)
+        {
+            if (activeShip.actionsLeft <= 0 || !objectManager.CanMoveObjectTo(activeShip, clickedHexagon)) return;
+            /*
+                         * rotate
+                         * move
+                         * rotate back
+                         */
+            var rotateAngle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
+
+            RotateShip(rotateAngle);
+
+            var x1 = cMap.Cells[activeShip.boxId].CellCenter.X;
+            var y1 = cMap.Cells[activeShip.boxId].CellCenter.Y;
+            var x2 = selectedCell.CellCenter.X;
+            var y2 = selectedCell.CellCenter.Y;
+
+            var oldPoints = new List<PointF>(activeShip.PolygonPoints);
+
+            var range = (int) Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            var dx = range / 15;
+            int step = 15;
+
+            var deltax = (x2 - x1) / step;
+            var deltay = (y2 - y1) / step;
+
+            for (int count1 = 0; count1 < range - 10; count1 += dx)
+            {
+                for (var i = 0; i < activeShip.PolygonPoints.Count; i++)
+                {
+                    activeShip.PolygonPoints[i] = new PointF(activeShip.PolygonPoints[i].X + deltax,
+                        activeShip.PolygonPoints[i].Y + deltay);
+                }
+                Thread.Sleep(5);
+                UpdateUi();
+            }
+
+            activeShip.PolygonPoints = oldPoints;
+
+            activeShip.Move(cMap, activeShip.boxId, selectedCell.id);
+
+            RotateShip(-rotateAngle);
+
+            //boxDescription.Text = activeShip.Description;
+
+            if (activeShip.actionsLeft == 0) activeShip = null;
+            //UpdateUi();
+        }
+    }
+}
