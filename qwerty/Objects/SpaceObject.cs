7f55a5493925a303604c46427e25f95d26ebbc5e
Konstantins Franckevics <cucazik@inbox.lv>
2017-12-15 02:37:54 +0100

    Added object coordinates, changed player field type

--- qwerty/Objects/SpaceObject.cs
+++ qwerty/Objects/SpaceObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using Barbar.HexGrid;
 
 namespace qwerty.Objects
 {
@@ -11,8 +12,9 @@ namespace qwerty.Objects
     abstract class SpaceObject
     {
         public ObjectType objectType; // подробности смотри в константах
-        public int player; // 0,1,2 ..0 - нейтральные объекты 
+        public Player player; // 0,1,2 ..0 - нейтральные объекты 
         public int boxId; // ячейка, в которой находится
+        public CubeCoordinates ObjectCoordinates;
         public int maxHealth; // hit points
         public int currentHealth;
 
@@ -20,9 +22,6 @@ namespace qwerty.Objects
 
         public abstract void drawSpaceShit(ref CombatMap cMap, ref System.Drawing.Bitmap bmap);
 
-        //public List<int> xpoints = new List<int>();  // координаты 5 точек, описывающих внешний вид объекта
-        //public List<int> ypoints = new List<int>();  // вообще, по сути надо каждый типа корабля сделать классом, тогда 
-                                                     // можно будет внешний вид задавать прямо здесь, массивом Point[]
         public List<PointF> PolygonPoints;                                            
 
         public int maxActions; // максимальное количество действий на одном ходу
