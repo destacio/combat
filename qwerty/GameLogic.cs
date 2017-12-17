eea1a93cf04b31cbf96366ea7cbfbac3f9046db3
Konstantins Franckevics <cucazik@inbox.lv>
2017-12-17 18:49:00 +0100

    Added game controlling code, commented out not yet working code

--- qwerty/GameLogic.cs
+++ qwerty/GameLogic.cs
@@ -10,7 +10,12 @@ namespace qwerty
 {
     public class GameLogic
     {
-        private Ship activeShip;
+        private Ship activeShip
+        {
+            get { return objectManager.ActiveShip; }
+            set { objectManager.ActiveShip = value; }
+        }
+
         private readonly ObjectManager objectManager;
         private Player activePlayer = Player.FirstPlayer;
 
@@ -77,28 +82,28 @@ namespace qwerty
                 // another object is out of range or requires more energy than is left
                 return;
             }
-
-            var angle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
-
-            // поворачиваем корабль на угол angle
-            RotateShip(angle);
-
-            // отрисовка атаки
-            Thread.Sleep(150);
-
-            if (activeShip.Attack(cMap, selectedCell.id, ref combatBitmap, player, ref pictureMap))
-                UpdateShipCount();
-            UpdateUi();
-
-            // возвращаем корабль в исходное положение
-            RotateShip(-angle);
-
-            // убираем подсветку с корабля, если у него не осталось очков передвижений
-            if (activeShip.actionsLeft == 0)
-            {
-                activeShip = null;
-                UpdateUi();
-            }
+//
+//            var angle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
+//
+//            // поворачиваем корабль на угол angle
+//            RotateShip(angle);
+//
+//            // отрисовка атаки
+//            Thread.Sleep(150);
+//
+//            if (activeShip.Attack(cMap, selectedCell.id, ref combatBitmap, player, ref pictureMap))
+//                UpdateShipCount();
+//            UpdateUi();
+//
+//            // возвращаем корабль в исходное положение
+//            RotateShip(-angle);
+//
+//            // убираем подсветку с корабля, если у него не осталось очков передвижений
+//            if (activeShip.actionsLeft == 0)
+//            {
+//                activeShip = null;
+//                UpdateUi();
+//            }
         }
 
         private void MoveActiveShip(OffsetCoordinates clickedHexagon)
@@ -109,44 +114,62 @@ namespace qwerty
                          * move
                          * rotate back
                          */
-            var rotateAngle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
+//            var rotateAngle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
+//
+//            RotateShip(rotateAngle);
+//
+//            var x1 = cMap.Cells[activeShip.boxId].CellCenter.X;
+//            var y1 = cMap.Cells[activeShip.boxId].CellCenter.Y;
+//            var x2 = selectedCell.CellCenter.X;
+//            var y2 = selectedCell.CellCenter.Y;
+//
+//            var oldPoints = new List<PointF>(activeShip.PolygonPoints);
+//
+//            var range = (int) Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+//            var dx = range / 15;
+//            int step = 15;
+//
+//            var deltax = (x2 - x1) / step;
+//            var deltay = (y2 - y1) / step;
+//
+//            for (int count1 = 0; count1 < range - 10; count1 += dx)
+//            {
+//                for (var i = 0; i < activeShip.PolygonPoints.Count; i++)
+//                {
+//                    activeShip.PolygonPoints[i] = new PointF(activeShip.PolygonPoints[i].X + deltax,
+//                        activeShip.PolygonPoints[i].Y + deltay);
+//                }
+//                Thread.Sleep(5);
+//                UpdateUi();
+//            }
+//
+//            activeShip.PolygonPoints = oldPoints;
+//
+//            activeShip.Move(cMap, activeShip.boxId, selectedCell.id);
+//
+//            RotateShip(-rotateAngle);
+//
+//            //boxDescription.Text = activeShip.Description;
+//
+//            if (activeShip.actionsLeft == 0) activeShip = null;
+//            //UpdateUi();
+        }
 
-            RotateShip(rotateAngle);
+        public void EndTurn()
+        {
+//            if (!UpdateShipCount())
+//            {
+//                return;
+//            }
 
-            var x1 = cMap.Cells[activeShip.boxId].CellCenter.X;
-            var y1 = cMap.Cells[activeShip.boxId].CellCenter.Y;
-            var x2 = selectedCell.CellCenter.X;
-            var y2 = selectedCell.CellCenter.Y;
+            activePlayer = activePlayer == Player.FirstPlayer ? Player.SecondPlayer : Player.FirstPlayer;
 
-            var oldPoints = new List<PointF>(activeShip.PolygonPoints);
+            //lblTurn.Text = "Ходит " + _activePlayer + "-й игрок";
 
-            var range = (int) Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            var dx = range / 15;
-            int step = 15;
+            activeShip = null;
 
-            var deltax = (x2 - x1) / step;
-            var deltay = (y2 - y1) / step;
+            objectManager.EndTurn();
 
-            for (int count1 = 0; count1 < range - 10; count1 += dx)
-            {
-                for (var i = 0; i < activeShip.PolygonPoints.Count; i++)
-                {
-                    activeShip.PolygonPoints[i] = new PointF(activeShip.PolygonPoints[i].X + deltax,
-                        activeShip.PolygonPoints[i].Y + deltay);
-                }
-                Thread.Sleep(5);
-                UpdateUi();
-            }
-
-            activeShip.PolygonPoints = oldPoints;
-
-            activeShip.Move(cMap, activeShip.boxId, selectedCell.id);
-
-            RotateShip(-rotateAngle);
-
-            //boxDescription.Text = activeShip.Description;
-
-            if (activeShip.actionsLeft == 0) activeShip = null;
             //UpdateUi();
         }
     }
