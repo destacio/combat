003505b3dd0637ba75c4bc9c5524eeb76e7123fd
Konstantins Franckevics <cucazik@inbox.lv>
2018-01-28 19:33:54 +0100

    Rewritten attack and movement sequences

--- qwerty/GameLogic.cs
+++ qwerty/GameLogic.cs
@@ -99,33 +99,21 @@ namespace qwerty
                 return;
             }
             
+            var rotateAngle = this.objectManager.GetTargetHexagonAngle(activeShip.ObjectCoordinates, enemyObject.ObjectCoordinates);
+            objectManager.RotateObject(activeShip, rotateAngle);
+            
             objectManager.AttackObject(activeShip, enemyObject);
 
             // TODO: move methods to object manager
             DealDamage(enemyObject, activeShip.AttackDamage);
             activeShip.actionsLeft--;
-//
-//            var angle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
-//
-//            // поворачиваем корабль на угол angle
-//            RotateShip(angle);
-//
-//            // отрисовка атаки
-//            Thread.Sleep(150);
-//
-//            if (activeShip.Attack(cMap, selectedCell.id, ref combatBitmap, player, ref pictureMap))
-//                UpdateShipCount();
-//            UpdateUi();
-//
-//            // возвращаем корабль в исходное положение
-//            RotateShip(-angle);
-//
-//            // убираем подсветку с корабля, если у него не осталось очков передвижений
-//            if (activeShip.actionsLeft == 0)
-//            {
-//                activeShip = null;
-//                UpdateUi();
-//            }
+
+            objectManager.RotateObject(activeShip, -rotateAngle);
+
+            if (activeShip.actionsLeft == 0)
+            {
+                activeShip = null;
+            }
         }
 
         private void DealDamage(SpaceObject victim, int damageAmount)
@@ -140,54 +128,19 @@ namespace qwerty
         private void MoveActiveShip(OffsetCoordinates clickedHexagon)
         {
             if (activeShip.actionsLeft <= 0 || !objectManager.CanMoveObjectTo(activeShip, clickedHexagon)) return;
-            /*
-             * rotate
-             * move
-             * rotate back
-             */
 
+            var rotateAngle = this.objectManager.GetTargetHexagonAngle(activeShip.ObjectCoordinates, clickedHexagon);
+            objectManager.RotateObject(activeShip, rotateAngle);
+            
             objectManager.MoveObjectTo(activeShip, clickedHexagon);
             activeShip.actionsLeft--;
 
-//            var rotateAngle = cMap.GetAngle(activeShip.boxId, selectedCell.id, activePlayer);
-//
-//            RotateShip(rotateAngle);
-//
-//            var x1 = cMap.Cells[activeShip.boxId].CellCenter.X;
-//            var y1 = cMap.Cells[activeShip.boxId].CellCenter.Y;
-//            var x2 = selectedCell.CellCenter.X;
-//            var y2 = selectedCell.CellCenter.Y;
-//
-//            var oldPoints = new List<PointF>(activeShip.PolygonPoints);
-//
-//            var range = (int) Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-//            var dx = range / 15;
-//            int step = 15;
-//
-//            var deltax = (x2 - x1) / step;
-//            var deltay = (y2 - y1) / step;
-//
-//            for (int count1 = 0; count1 < range - 10; count1 += dx)
-//            {
-//                for (var i = 0; i < activeShip.PolygonPoints.Count; i++)
-//                {
-//                    activeShip.PolygonPoints[i] = new PointF(activeShip.PolygonPoints[i].X + deltax,
-//                        activeShip.PolygonPoints[i].Y + deltay);
-//                }
-//                Thread.Sleep(5);
-//                UpdateUi();
-//            }
-//
-//            activeShip.PolygonPoints = oldPoints;
-//
-//            activeShip.Move(cMap, activeShip.boxId, selectedCell.id);
-//
-//            RotateShip(-rotateAngle);
-//
-//            //boxDescription.Text = activeShip.Description;
-//
-//            if (activeShip.actionsLeft == 0) activeShip = null;
-//            //UpdateUi();
+            objectManager.RotateObject(this.activeShip, -rotateAngle);
+
+            if (activeShip.actionsLeft == 0)
+            {
+                activeShip = null;
+            }
         }
         
         private void MoveMeteors()
